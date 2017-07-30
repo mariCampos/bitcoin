67a61470d4b6121c1627dd8f4eefeba23cc798d8
Mariana <marianaalbuquerque02@gmail.com>
2017-07-30 16:53:00 -0300

    Export modules bitcoinlib

--- src/index.js
+++ src/index.js
@@ -3,14 +3,13 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
-module.exports = {
-  base58: require('bs58'),
-  bitcoin: require('bitcoinjs-lib'),
-  ecurve: require('ecurve'),
-  BigInteger: require('bigi'),
-  Buffer: require('buffer'),
-  bigi: require('bigi')
-}
+// module.exports = {
+//   base58: require('bs58'),
+//   bitcoin: require('bitcoinjs-lib'),
+//   BigInteger: require('bigi'),
+//   Buffer: require('buffer'),
+//   bigi: require('bigi')
+// }
 
 ReactDOM.render(
   <App />,
