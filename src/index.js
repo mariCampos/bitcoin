73bfb3a7ae667631bec35a23fee01e7173d95c16
Mariana <marianaalbuquerque02@gmail.com>
2017-07-30 16:47:59 -0300

    Export modules

--- src/index.js
+++ src/index.js
@@ -3,6 +3,15 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
+module.exports = {
+  base58: require('bs58'),
+  bitcoin: require('bitcoinjs-lib'),
+  ecurve: require('ecurve'),
+  BigInteger: require('bigi'),
+  Buffer: require('buffer'),
+  bigi: require('bigi')
+}
+
 ReactDOM.render(
   <App />,
   document.getElementById('root')
